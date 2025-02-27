eb4e87518d8371169fc1bb2ae61b70a0cb4e441e
psjadhav19teen <42905297+psjadhav19teen@users.noreply.github.com>
2025-02-27 08:21:12 +0000

    second commit

--- flipkartproject/app/urls.py
+++ flipkartproject/app/urls.py
@@ -15,4 +15,11 @@ urlpatterns = [
     path("reset_password/<uname>/", views.reset_password, name="reset_password"),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
+    path('mobilelist/',views.mobilelist,name='mobilelist'),
+    path('clothlist/',views.clothlist,name='clothlist'),
+    path('shoeslist/',views.shoeslist,name='shoeslist'),
+    path('electronicslist/',views.electronicslist,name='electronicslist'),
+    path('showpricerange/',views.showpricerange,name='showpricerange'),
+    path('sortingbyprice/',views.sortingbyprice,name='sortingbyprice'),
+    path('searchproduct/',views.searchproduct,name='searchproduct')
 ]
